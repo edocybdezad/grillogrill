d714708a3fb00df75ed36d1f8fe5a4021ed65318
edocybdezad <luis.alttamirano@gmail.com>
2021-10-08 12:46:40 +0200

    Update project

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import include,path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView,LogoutView
 from . import views
 from .views import ItemUpdate
  
@@ -7,6 +7,7 @@ urlpatterns = [
     path('',views.index, name='index'),
     path('manage/',views.manage, name='manage'),
     path("<int:pk>", ItemUpdate.as_view(), name="item-update"),
-    path("login/", LoginView.as_view(template_name='homepage/login.html'), name="login"),    
+    path("login/", LoginView.as_view(template_name='homepage/login.html'), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),  
     # path('import',views.menu_import, name='import'),
 ]
