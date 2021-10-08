1769c9c4316c762e5cdfabbab8ade4ad72fe1451
edocybdezad <luis.alttamirano@gmail.com>
2021-10-08 14:54:20 +0200

    secrey to literal

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
-
+SECRET_KEY = 'django-insecure-e&c%ob2+)@g1!z%qup=6a)ohy=f!7ur2i1268iemxpopzpm4zy'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
